3d4239c87b7a0c8081e2f156a73bd2c73df25d15
Sukhjinder-Kaur <sukhmanyu@gmail.com>
2020-09-18 15:24:27 -0400

    Add project files.

---
+++ test.js
@@ -0,0 +1,8 @@
+﻿(function ()
+{
+
+	var pText = document.createTextNode("This is a Git repository demo by Sukhjinder Kaur");
+	var divContainer = document.getElementById("demo");
+	divContainer.appendChild(pText);
+
+})();
